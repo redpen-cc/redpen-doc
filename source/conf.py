da3aa43d8cbbfe5598276935fd190b86331fd281
takahi-i <takahiko03@gmail.com>
2014-04-08 14:47:31 +0900

    Add navi-bar

--- source/conf.py
+++ source/conf.py
@@ -97,7 +97,7 @@ html_theme = 'dv'
 # further.  For a list of options available for each theme, see the
 # documentation.
 html_theme_options = {
-    "linkcolor" : "green",
+    "linkcolor" : "black",
     "headerlinkcolor" : "#7F0120",
     "textalign" : "left"
     }
