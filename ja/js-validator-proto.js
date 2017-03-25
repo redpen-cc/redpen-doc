d3e923d2fa8cb1b118fe0f84bd0038bf5e97b807
takahi-i <takahiko03@gmail.com>
2017-03-25 23:50:58 +0900

    Port Japanese document to ja

---
+++ ja/js-validator-proto.js
@@ -0,0 +1,20 @@
+function preValidateSentence(sentence) {
+}
+
+function preValidateSection(section) {
+}
+
+function validateDocument(document) {
+  // your validation logic for document here
+}
+
+function validateSentence(sentence) {
+  // if ( ... sentenceについて検査を実行 ... ) {
+  //   // 検査に失敗
+  //   addError('エラーについて報告', sentence);
+  // }
+}
+
+function validateSection(section) {
+  // sectionに対する検査処理を記述する
+}
